80862f18cc9f5b2f413b430a724361e606e5b4b0
mellamochiao <chou6855@gmail.com>
2025-06-29 00:39:50 +0800

    update

--- StepByStepLinearRegression/simplelinear.py
+++ StepByStepLinearRegression/simplelinear.py
@@ -8,10 +8,16 @@ class SimpleLinearRegression:
         self.slope = None
         self.intercept = None
         self.steps_df = None
+        self.sse = None
+        self.ssr = None
+        self.mse = None
+        self.n = None
+
 
     def fit(self, x, y):
         x = np.array(x)
         y = np.array(y)
+        self.n = len(x)
 
         self.x_bar = np.mean(x)
         self.y_bar = np.mean(y)
@@ -29,6 +35,11 @@ class SimpleLinearRegression:
 
         y_hat = self.intercept + self.slope * x
         residuals = y - y_hat
+        y_hat_dev = y_hat - self.y_bar
+
+        self.sse = np.sum(residuals ** 2)
+        self.mse = self.sse / (len(x)-2)        
+        self.ssr = np.sum(y_hat_dev ** 2)
 
         self.steps_df = pd.DataFrame({
             'x': x,
@@ -38,13 +49,17 @@ class SimpleLinearRegression:
             '(x - x̄)(y - ȳ)': xy_dev,
             '(x - x̄)^2': xx_dev,
             'ŷ': y_hat,
-            'residual': residuals
+            'residual': residuals,
+            'ŷ - ȳ': y_hat_dev
         })
 
     def summary(self):
         print(f"x̄ = {self.x_bar:.4f}, ȳ = {self.y_bar:.4f}")
         print(f"Slope (b1) = {self.slope:.4f}")
         print(f"Intercept (b0) = {self.intercept:.4f}")
+        print(f"SSR = {self.ssr:.4f}")
+        print(f"SSE = {self.sse:.4f}")
+        print(f"MSE = {self.mse:.4f}")
 
     def show_steps(self):
         print(self.steps_df.to_string(index=False))
